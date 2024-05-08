feecc07f94e39eedb4a8b8e954fc9224006924f8
Shailendra Kumar <121594314+GitShailendra@users.noreply.github.com>
2024-05-08 22:04:31 +0530

    Add files via upload
    Calculator Project

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
+// let input = document.getElementById('inputBox');
+// let buttons = document.querySelectorAll('button');
 
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
+// let string="";
+// let arr = Array.from(buttons);
 
-var main = document.querySelector('#main');
-var cursor = document.querySelector('.cursor');
+// arr.forEach(button => {
+//     button.addEventListener('click',(e)=>{
+//         if(e.target.innerHTML == '='){
+//             string = eval(string);
+//             console.log(string);
 
-main.addEventListener("mousemove",function(dets){
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y,
-        duration:1
-    });
+//             input.value = string;
+//         }
+//           else if(e.target.innerHTML =='AC'){
+//             string = "";
+//             input.value = string;
+//           }
+//           else if(e.target.innerHTML == 'DEL'){
+//             string = string.substring(0,string.length-1);
+//             console.log(string);
+//             input.value = string
+//           }
+//           else{
+
+//         string += e.target.innerHTML;
+//         console.log(string);
+//         input.value = string;
+//           }
+
+
+
+//     })
+// })
+
+let buttons = document.querySelectorAll('button');
+let input = document.querySelector('input');
+let string = ""
+
+Array.from(buttons).forEach(function (buttons){
+  buttons.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      console.log(string);
+      input.value = string;
+      
+    }
+    else if(e.target.innerHTML=='AC'){
+      string  = "";
+      input.value = string;
+    }
+    else if(e.target.innerHTML=="DEL"){
+      string = string.substring(0,string.length-1)
+      
+      input.value = string;
+    }
+    else{
+    
+    
+    console.log(e.target);
+    string = string + e.target.innerHTML;
+    input.value = string;
+  }
+  })
 })
